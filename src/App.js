bddc65eab6261f783f9bcad73e77b8c26c985cb9
Nooshin Attarian <noshinatarian@gmail.com>
2021-01-16 16:31:49 -0500

    added more style to app.css

--- src/App.js
+++ src/App.js
@@ -12,9 +12,16 @@ class App extends Component {
     };
 
     return (
-      <div>
-        <Flat flat={flat} />
+      <div className="app">
+        <div className="main">
+          <div className="search">
+          </div>
+          <div className="flats">
+          </div>
+        </div>
+        <div className="map">
       </div>
+    </div>
     );
   }
 }
