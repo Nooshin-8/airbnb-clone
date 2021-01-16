58f2c15f6affb55348b2e796d4935c68d06d9eba
Nooshin Attarian <noshinatarian@gmail.com>
2021-01-16 15:25:37 -0500

    flat component

--- src/components/flat.js
+++ src/components/flat.js
@@ -1,11 +1,16 @@
 import React from "react";
+import "./flat.css"
 
 class Flat extends React.Component {
   render() {
+    const title = this.props.flat.price
+    this.props.flat.priceCurrency + " - " + this.props.flat.name;
+
     return (
       <div className="flat">
-        <div className="flat-picture"> </div>
-        <divclassName="flat-title"> </div>
+        <div className="flat-picture"></div>
+        <div className="flat-title"></div>
+        {title}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Hello from './components/hello';
+import Flat from './components/flat';
 
 ReactDOM.render(
   <React.StrictMode>
