886a0e8a39bee809a128dcd7f1c0a46fa8bbecbc
Nooshin Attarian <noshinatarian@gmail.com>
2021-01-16 11:22:03 -0500

    created the first component

--- src/components/hello.js
+++ src/components/hello.js
@@ -1,7 +1,11 @@
 import React from "react";   //importing react class from react package
 
-class Hello extends React.component {
+class Hello extends React.Component {
   render() {
-
+    return <div>Hello from hello.js</div>
   }
 }
+
+// the goal of render method is creat a string/html and return it
+
+export default Hello;
